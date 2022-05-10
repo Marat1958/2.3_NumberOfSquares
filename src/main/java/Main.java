4e1c960fd2c1a700b64b59800833920e8548c863
Marat Safiyulin <marat.safiyulin@mail.ru>
2022-05-10 17:45:56 +0300

    Add first commit for 2.3_NumberOfSquares

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import ru.netology.sqr.SQRService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SQRService service = new SQRService();
+        int res = service.squareCount(200, 300);
+        System.out.println(res);
+
+    }
+}
---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,15 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SQRServiceTest {
+    @Test
+    public void shouldSquareCountBetweenSqrMinAndSqrMax() {
+        SQRService service = new SQRService();
+        int expected = 3;
+        int actual = service.squareCount(200, 300);
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
